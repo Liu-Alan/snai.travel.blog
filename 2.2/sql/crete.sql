b02ccdd87e7ec2ed57223c0623ce1adeda24d9eb
Liu-Alan <xiaolin-liu@qq.com>
2022-01-06 01:54:26 +0800

    出包

--- 2.2/sql/crete.sql
+++ 2.2/sql/crete.sql
@@ -1,27 +1,53 @@
-CREATE DATABASE blog_service CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+CREATE DATABASE blog_service CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 ;
 
 USE blog_service
 ;
 
--- drop TABLE game;
-CREATE TABLE game(
-	`game_id` INT AUTO_INCREMENT PRIMARY KEY,			-- 自增列需为主键
-	`game_name` NVARCHAR(32) NOT NULL COMMENT '游戏名称',
-	`game_alias` VARCHAR(32) NOT NULL COMMENT '游戏别名',
-    `status` smallint NOT NULL DEFAULT 1 COMMENT '0 禁用, 1 只显示A, 2 根据判断显示A或B',
-    `app_id` varchar(32) NOT NULL DEFAULT '' COMMENT '用户接口AppId',
-    `app_key` varchar(32) NOT NULL DEFAULT '' COMMENT '用户接口AppKey',
-    `create_time` INT NOT NULL DEFAULT 0 COMMENT '创建时间'
-)
-;
-ALTER TABLE game ADD UNIQUE INDEX ix_game_game_name(game_name)
-;
-ALTER TABLE game ADD UNIQUE INDEX ix_game_game_alias(game_alias)
-;
-ALTER TABLE game ADD UNIQUE INDEX ix_game_app_id(app_id)
-;
+-- drop TABLE blog_tag;
+CREATE TABLE `blog_tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT '' COMMENT '标签名称',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(64) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(64) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT '删除时间',
+  `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0 为未删除、1 为已删除',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0 为禁用、1 为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='标签管理';
+ALTER TABLE blog_tag ADD UNIQUE INDEX ix_blog_tag_name(`name`);
 
-INSERT into game(`game_name`,`game_alias`,`status`,`app_id`,`app_key`,`create_time`)
-VALUES('Teen Patti Sky','TeenPattiSky',2,'e68C0X3aLrw9PBVT','kQK3Y0RUsgDwOHzg8vGymh51EvVMSdy6',1639729728)
-;
+-- drop TABLE blog_article;
+CREATE TABLE `blog_article` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) DEFAULT '' COMMENT '文章标题',
+  `desc` varchar(255) DEFAULT '' COMMENT '文章简述',
+  `cover_image_url` varchar(255) DEFAULT '' COMMENT '封面图片地址',
+  `content` longtext COMMENT '文章内容',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT '删除时间',
+  `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0 为未删除、1 为已删除',
+  `state` tinyint(3) unsigned DEFAULT '1' COMMENT '状态 0 为禁用、1 为启用',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章管理';
+ALTER TABLE blog_article ADD INDEX ix_blog_article_title(`title`);
+
+-- drop TABLE blog_article_tag;
+CREATE TABLE `blog_article_tag` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) NOT NULL COMMENT '文章 ID',
+  `tag_id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '标签 ID',
+  `created_on` int(10) unsigned DEFAULT '0' COMMENT '创建时间',
+  `created_by` varchar(100) DEFAULT '' COMMENT '创建人',
+  `modified_on` int(10) unsigned DEFAULT '0' COMMENT '修改时间',
+  `modified_by` varchar(100) DEFAULT '' COMMENT '修改人',
+  `deleted_on` int(10) unsigned DEFAULT '0' COMMENT '删除时间',
+  `is_del` tinyint(3) unsigned DEFAULT '0' COMMENT '是否删除 0 为未删除、1 为已删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='文章标签关联';
+ALTER TABLE blog_article_tag ADD UNIQUE INDEX ix_blog_article_tag_article_id_tag_id(`article_id`,`tag_id`);
