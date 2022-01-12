623ad421cda3733adafbd33dc79c4c55083971ac
Liu-Alan <xiaolin-liu@qq.com>
2022-01-12 22:59:47 +0800

    2.3 setting

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "snai.travel.blog/2.2/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
--- main.go
+++ main.go
@@ -1,11 +1,48 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"snai.travel.blog/2.2/global"
+	"snai.travel.blog/2.2/pkg/setting"
 	"snai.travel.blog/internal/routers"
 )
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+
+	return nil
+}
 
 func main() {
 	router := routers.NewRouter()
