d06ad61615254393ed01e2a74b703a01368d83dd
Liu-Alan <xiaolin-liu@qq.com>
2022-02-13 12:07:16 +0800

    convert pagination

---
+++ pkg/app/pagination.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+	"snai.travel.blog/global"
+	"snai.travel.blog/pkg/convert"
+)
+
+func GetPage(c *gin.Context) int {
+	page := convert.StrTo(c.Query("page")).MustInt()
+	if page <= 0 {
+		return 1
+	}
+
+	return page
+}
+
+func GetPageSize(c *gin.Context) int {
+	pageSize := convert.StrTo(c.Query("page_size")).MustInt()
+	if pageSize <= 0 {
+		return global.AppSetting.DefaultPageSize
+	}
+	if pageSize > global.AppSetting.MaxPageSize {
+		return global.AppSetting.MaxPageSize
+	}
+
+	return pageSize
+}
