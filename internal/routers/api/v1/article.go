7d015e077ab7804e1e058292817fa646abdd77f6
Liu-Alan <xiaolin-liu@qq.com>
2022-03-08 01:09:22 +0800

    test

--- internal/routers/api/v1/article.go
+++ internal/routers/api/v1/article.go
@@ -1,6 +1,10 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"snai.travel.blog/pkg/app"
+	"snai.travel.blog/pkg/errcode"
+)
 
 type Article struct{}
 
@@ -8,7 +12,9 @@ func NewArticle() Article {
 	return Article{}
 }
 
-func (a Article) Get(c *gin.Context)    {}
+func (a Article) Get(c *gin.Context) {
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+}
 func (a Article) List(c *gin.Context)   {}
 func (a Article) Create(c *gin.Context) {}
 func (a Article) Update(c *gin.Context) {}
