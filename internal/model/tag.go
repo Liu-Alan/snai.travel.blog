40c5541ff35465bb77e8b9ffba87ea1b41ec254b
Liu-Alan <xiaolin-liu@qq.com>
2022-04-09 16:11:51 +0800

    tag model

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,5 +1,7 @@
 package model
 
+import "github.com/jinzhu/gorm"
+
 type Tag struct {
 	*Model
 	Name  string `json:"name"`
@@ -8,4 +10,32 @@ type Tag struct {
 
 func (t Tag) TableName() string {
 	return `blog_tag`
+}
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name=?", t.Name)
+	}
+	db = db.Where("state=?", t.State)
+	if err := db.Model(&t).Where("is_del=?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+	var err error
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+	if t.Name != "" {
+		db = db.Where("name=?", t.Name)
+	}
+	db = db.Where("state=?", t.State)
+	if err = db.Where("is_del=?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+	return tags, nil
 }
