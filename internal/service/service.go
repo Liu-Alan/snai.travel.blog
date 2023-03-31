fc69b16477031d9df7ae667e1c1a1370cb3aca75
Liu-Alan <xiaolin-liu@qq.com>
2023-03-31 17:59:32 +0800

    2.6.4 新建 service 方法

---
+++ internal/service/service.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"context"
+
+	"snai.travel.blog/global"
+	"snai.travel.blog/internal/dao"
+)
+
+type Service struct {
+	ctx context.Context
+	dao *dao.Dao
+}
+
+func New(ctx context.Context) Service {
+	svc := Service{ctx: ctx}
+	svc.dao = dao.New(global.DBEngine)
+	return svc
+}
