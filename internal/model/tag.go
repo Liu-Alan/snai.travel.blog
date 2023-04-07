bfd9b78e7709c784df435f2fe188a6bafb1eaaa8
Liu-Alan <xiaolin-liu@qq.com>
2023-04-07 20:51:38 +0800

    完成标签管理

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -44,8 +44,12 @@ func (t Tag) Create(db *gorm.DB) error {
 	return db.Create(&t).Error
 }
 
-func (t Tag) Update(db *gorm.DB) error {
-	return db.Model(&Tag{}).Where("id = ? AND is_del=?", t.ID, 0).Update(t).Error
+func (t Tag) Update(db *gorm.DB, values interface{}) error {
+	if err := db.Model(t).Where("id = ? AND is_del = ?", t.ID, 0).Updates(values).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (t Tag) Delete(db *gorm.DB) error {
