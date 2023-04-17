3a64e85f14438442ba7bf9ef11079bf9ed47b61d
Liu-Alan <xiaolin-liu@qq.com>
2023-04-18 07:48:28 +0800

    2.8 对接口进行访问控制 jwt

--- pkg/app/jwt.go
+++ pkg/app/jwt.go
@@ -1,8 +1,11 @@
 package app
 
 import (
+	"time"
+
 	"github.com/dgrijalva/jwt-go"
 	"snai.travel.blog/global"
+	"snai.travel.blog/pkg/util"
 )
 
 type Claims struct {
@@ -13,4 +16,21 @@ type Claims struct {
 
 func GetJWTSecret() []byte {
 	return []byte(global.JWTSetting.Secret)
+}
+
+func GenerateToken(appKey, appSecret string) (string, error) {
+	nowTime := time.Now()
+	expireTime := nowTime.Add(global.JWTSetting.Expire)
+	claims := Claims{
+		AppKey:    util.EncodeMD5(appKey),
+		AppSecret: util.EncodeMD5(appSecret),
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime.Unix(),
+			Issuer:    global.JWTSetting.Issuer,
+		},
+	}
+
+	tokenClaims := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := tokenClaims.SignedString(GetJWTSecret())
+	return token, err
 }
