72e37332128e28d06ae0538c589eafd7dc44c4c0
Liu-Alan <xiaolin-liu@qq.com>
2023-04-27 19:47:30 +0800

    中间件 日志

---
+++ internal/middleware/access_log.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"bytes"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AccessLogWriter struct {
+	gin.ResponseWriter
+	body *bytes.Buffer
+}
+
+func (w AccessLogWriter) Write(p []byte) (int, error) {
+	if n, err := w.body.Write(p); err != nil {
+		return n, err
+	}
+
+	return w.ResponseWriter.Write(p)
+}
